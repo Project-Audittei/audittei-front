861a7c1414f9b5637b66ff1bd1aeda0359948bbb
Fernando Avila <fernandoavilajunior@gmail.com>
2024-05-13 19:49:52 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import PaginaTipografia from "./pages/PaginaTipografia";
 
 function App() {
   return (
-    <BrowserRouter basename="https://rodrigoveiga.com.br/audittei">
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={ <PaginaTipografia /> } />
       </Routes>
