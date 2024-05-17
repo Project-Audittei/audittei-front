6e73c72b44a619f7d39e3d7f8af0309c4387dc64
Fernando Ávila <fernandoavilajunior@gmail.com>
2024-05-17 20:08:58 -0300

    fix

--- src/components/Paginacao/Paginacao.tsx
+++ src/components/Paginacao/Paginacao.tsx
@@ -1,5 +1,5 @@
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export interface IPaginacao {
     atual: number;
@@ -7,34 +7,30 @@ export interface IPaginacao {
 }
 
 export default function Paginacao({ atual, quantidade }: IPaginacao) {
-    const [paginaAtual, setPaginaAtual] = useState<number>(atual);
-    const [itens, setItens] = useState<JSX.Element[]>([]);
+    const [itens] = useState<JSX.Element[]>([]);
     const quantidadeMinimaEmTela = 3;
     
-    useEffect(() => {
-        if(atual > quantidadeMinimaEmTela) {
-            itens.push(<li className={`pagina-item mais`}>1</li>);
-            itens.push(<li className={`pagina-item mais`}>...</li>);
-        }
-        
-        if (atual < quantidade) {
-            for (let index = atual - 1; index <= atual + 1; index++) {
-                console.log('executando...');
-                if(quantidade > 5 && index > atual + 2) {
-                    itens.push(<li key={ index } className={`pagina-item mais`}>...</li>);
-                    itens.push(<li key={ index } className={`pagina-item`}>{ quantidade }</li>);
-                    break;
-                }
-                itens.push(<li key={ index } className={`pagina-item ${ index === atual ? 'ativo' : '' }`}>{ index }</li>);
-            }
-        }
+    if(atual > quantidadeMinimaEmTela) {
+        itens.push(<li className={`pagina-item mais`}>1</li>);
+        itens.push(<li className={`pagina-item mais`}>...</li>);
+    }
     
-        if ( atual + 3 > quantidadeMinimaEmTela) {
-            itens.push(<li className={`pagina-item mais`}>...</li>);
-            itens.push(<li className={`pagina-item mais`}>{ quantidade }</li>);
+    if (atual < quantidade) {
+        for (let index = atual - 1; index <= atual + 1; index++) {
+            console.log('executando...');
+            if(quantidade > 5 && index > atual + 2) {
+                itens.push(<li key={ index } className={`pagina-item mais`}>...</li>);
+                itens.push(<li key={ index } className={`pagina-item`}>{ quantidade }</li>);
+                break;
+            }
+            itens.push(<li key={ index } className={`pagina-item ${ index === atual ? 'ativo' : '' }`}>{ index }</li>);
         }
-        console.log(itens);
-    }, [paginaAtual]);
+    }
+
+    if ( atual + 3 > quantidadeMinimaEmTela) {
+        itens.push(<li className={`pagina-item mais`}>...</li>);
+        itens.push(<li className={`pagina-item mais`}>{ quantidade }</li>);
+    }
 
     return (
         <div className="paginacao">
