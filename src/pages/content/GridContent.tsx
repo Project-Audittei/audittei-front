263c67988168a14ef6f1c1a1dbcd22e0d6078486
Fernando Ávila <fernandoavilajunior@gmail.com>
2024-05-18 02:49:19 -0300

    grid

--- src/pages/content/GridContent.tsx
+++ src/pages/content/GridContent.tsx
@@ -1,13 +1,56 @@
-import { Home } from "lucide-react";
-import Botao from "../../components/Botoes/Botao";
-
 export default function GridContent() {
     return (
         <>
-            <h1>Grid</h1>
-            <div className="row" style={{ height: '100%' }}>
+            <h1 className="titulo-pagina">01. Grid</h1>
+            <div className="row">
                 <div className="col">
-                    <Botao estilo="Primary" tamanho="Normal" label="Botão" icone={ <Home /> } iconePosicao="direita" />
+                    <div className="row mb-2" style={{ height: '100%' }}>
+                    <div className="col-3" style={{ height: 750 }}>
+                        <div className="subtitulo">320-640 <span>Celular - Centralizado</span></div>
+                        <div className="row demo-grid celular">
+                            <div className="col"></div>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                        </div>
+                    </div>
+                    <div className="col-7 ml-4" style={{ height: 750 }}>
+                        <div className="subtitulo">768 até 1024 <span>Tablet - Centralizado</span></div>
+                        <div className="row demo-grid tablet">
+                            <aside><div className="content"></div></aside>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row" style={{ height: '100%' }}>
+                    <div className="col-11" style={{ maxHeight: 750 }}>
+                        <div className="subtitulo">1280 até 1920 <span>Desktop - Centralizado</span></div>
+                        <div className="row demo-grid desktop">
+                            <aside><div className="content"></div></aside>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                            <div className="col"></div>
+                        </div>
+                    </div>
+                </div>
                 </div>
             </div>
         </>
