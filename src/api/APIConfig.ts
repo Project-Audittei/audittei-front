5c7ad36b089b821ec84d841f17dc8a41732e8790
Fernando Avila <fernandoavilajunior@gmail.com>
2024-06-18 21:22:54 -0300

    ajuste rota api

--- src/api/APIConfig.ts
+++ src/api/APIConfig.ts
@@ -1,4 +1,4 @@
 export const APIConfig = {
-    // baseUrl: "https://api.audittei.com.br"
-    baseUrl: "https://localhost:8000/api"
+    baseUrl: "https://api.audittei.com.br"
+    //baseUrl: "https://localhost:8000/api"
 };
