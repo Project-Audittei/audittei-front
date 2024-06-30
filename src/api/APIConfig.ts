5fbe812413e9f1945be4609b95abbbf21c7153a3
Fernando Avila <fernandoavilajunior@gmail.com>
2024-06-30 11:36:31 -0300

    Ajuste na rota da api

--- src/api/APIConfig.ts
+++ src/api/APIConfig.ts
@@ -1,7 +1,7 @@
 export const APIConfig = {
     // apontando para a API de Homologação
-    // baseUrl: "https://auditteiapi.ofernandoavila.com/api",
-    baseUrl: "http://localhost:8000/api",
+    baseUrl: "https://auditteiapi.ofernandoavila.com/api",
+    // baseUrl: "http://localhost:8000/api",
 
     // Autenticação
     cadastro: '/auth/cadastro',
